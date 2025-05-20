9e844b926706bcb1b3c1f29f784d13e57fea4b5c
ALONE-VIBER <kuttypulisurya22@gmail.com>
2025-05-20 17:36:00 +0530

    create an code to add an new todo list . issue the js functions are not working for new todo's

--- Todo/script.js
+++ Todo/script.js
@@ -1,8 +1,9 @@
-//const todoList = document.querySelector("#todo-list");
-const todos = document.querySelectorAll("#todo-list .todo-container");
+const todoList = document.querySelector("#todo-list");
+const todos = todoList.querySelectorAll(".todo-container");
 
 todos.forEach(function (todoContainer) {
 
+    console.log(todoContainer)
     const content = todoContainer.querySelector(".content");
     const para = content.querySelector("p");
     const editButton = todoContainer.querySelector("button");
@@ -39,4 +40,48 @@ todos.forEach(function (todoContainer) {
     })
 })
 
+const addTodo = document.querySelector("#add-todo");
+const addTodoButton = addTodo.querySelector("button");
 
+addTodoButton.addEventListener("click",()=>{
+
+    const divContainer = document.createElement("div");
+    divContainer.className = "todo-container";
+
+    const divTodo = document.createElement("div");
+    divTodo.className = "todo";
+
+    const divCheckBox = document.createElement("div");
+    divCheckBox.className = "check-box";
+
+    const divContent = document.createElement("div");
+    divContent.className = "content";
+    divContent.textContent = "☐";
+    
+    const userInput = addTodo.querySelector("#new-todo");
+    const  p = document.createElement("p");
+    p.textContent = userInput.value;
+
+    const editbutton = document.createElement("button");
+    editbutton.textContent = "EDIT";
+
+    const divEdit = document.createElement("div");
+    const input = document.createElement("input");
+    input.id = "new-todo";
+    const updatebutton = document.createElement("button");
+    updatebutton.textContent = "UPDATE";
+
+    divContent.appendChild(p);
+    divTodo.appendChild(divCheckBox);
+    divTodo.appendChild(divContent);
+    divContainer.appendChild(divTodo);
+
+    divContainer.appendChild(editbutton);
+
+    divEdit.appendChild(input);
+    divEdit.appendChild(updatebutton);
+    divContainer.appendChild(divEdit);
+
+    todoList.appendChild(divContainer);
+
+})
